c027e8777b148d6c88f0d8a9a2dad22b57a61619
zhaodaxing <358957476@qq.com>
2022-07-14 22:17:47 +0800

    Layers的添加

---
+++ TestEngine/Hazel/src/Hazel/Layer.cpp
@@ -0,0 +1,16 @@
+#include "hzpch.h"
+#include "Layer.h"
+
+namespace Hazel
+{
+	Layer::Layer(const std::string& name /*= "Layer"*/)
+		:m_DebugName(name)
+	{
+
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+}
---
+++ TestEngine/Hazel/src/Hazel/Layer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Hazel/Core.h"
+#include "Hazel/Events/Event.h"
+
+namespace Hazel
+{
+	class HAZEL_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {};
+		virtual void OnDetach() {};
+		virtual void OnUpdate() {};
+		virtual void OnEvent(Event& e) {};
+
+		inline std::string& GetName() { return m_DebugName; };
+	private:
+		std::string m_DebugName;
+	};
+}
