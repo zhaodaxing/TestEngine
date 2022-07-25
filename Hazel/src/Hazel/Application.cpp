61c08c220d312c9bd5bb1f28ffc2e2fab2b898d1
zhaohelei <zhaohelei@linctex.com>
2022-07-25 13:56:54 +0800

    fix: PushOverlay逻辑错误修复， 增加Window, App接口

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -9,8 +9,14 @@ namespace Hazel
 {
 	// bind(x, this, ..) this, 表示绑定当前实例的x函数, 可以产生多态
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+	Application* Application::s_Instance = nullptr;
+
 	Application::Application()
 	{
+		HZ_CORE_ASSERT(!s_Instance, "Applicatoin already exits!");
+		s_Instance = this;
+
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 
@@ -19,11 +25,13 @@ namespace Hazel
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
-		m_LayerStack.PopOverlay(layer);
+		m_LayerStack.PushOverlay(layer);
+		layer->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "Core.h"
 
@@ -22,12 +22,19 @@ namespace Hazel
 
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
+
+		inline static Application& Get() {return *s_Instance; }
+		inline Window& GetWindow() {return *m_Window; }
+	
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+	
+	private:
+		static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
