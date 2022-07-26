6354c98304ea7bcad49c5a994d7fce62e528cb06
zhaohelei <zhaohelei@linctex.com>
2022-07-26 13:51:57 +0800

    fix: StartProject fix

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "Hazel"
     architecture "x64"
+    startproject "Sandbox"
     configurations
     {
         "Debug",
@@ -19,7 +20,7 @@ include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
 include "Hazel/vendor/imgui"
 
-startproject "Sandbox"
+
 
 project "Hazel"
     location "Hazel"
