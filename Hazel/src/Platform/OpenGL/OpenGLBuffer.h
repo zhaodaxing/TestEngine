ecef41b9a5623cd5296987d127026293b7926885
zhaohelei <zhaohelei@linctex.com>
2022-07-28 14:15:48 +0800

    feat: 29 Render API Abstraction

---
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "Hazel/Renderer/Buffer.h"
+
+namespace Hazel 
+{ 
+	class OpenGLVertexBuffer: public VertexBuffer
+	{
+	public:
+		OpenGLVertexBuffer(float* vertices, uint32_t size);
+		virtual ~OpenGLVertexBuffer();
+
+		void Bind() const override;
+		void UnBind() const override;
+	private:
+		uint32_t m_RendererID;
+	};
+
+	class OpenGLIndexBuffer : public IndexBuffer
+	{
+
+	public:
+		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
+		virtual ~OpenGLIndexBuffer();
+
+		virtual uint32_t GetCount() const { return m_Count; }
+
+		void Bind() const override;
+		void UnBind() const override;
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_Count;
+	};
+}
+
