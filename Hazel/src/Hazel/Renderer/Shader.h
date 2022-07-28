d18a53aeacb663defc102a674e0bd078d968c9e7
zhaohelei <zhaohelei@linctex.com>
2022-07-28 14:15:48 +0800

    feat: 28 OpenGL Shaders

---
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+namespace Hazel 
+{ 
+	class Shader
+	{
+	public:
+		Shader(const std::string& vertexSource, const std::string& fragmentSource);
+		~Shader();
+		void Bind();
+		void UnBind();
+
+	private:
+		unsigned int m_RendererID;
+	};
+
+}
